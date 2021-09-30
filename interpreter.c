201ff8f0830031a909d978c9cd49737662ee1de1
Athul Chandran <athul041@gmail.com>
2021-10-01 00:24:11 +0530

    Memory assigned

--- interpreter.c
+++ interpreter.c
@@ -13,24 +13,24 @@ void readFromFile(char fileName[], int* memPtr)         // Implement using loops
         long filesize = ftell(f1);
         // printf("\n%d",filesize);
         fseek(f1, 0 ,SEEK_SET);
-        int* buffer = malloc(filesize);
-        // fread(*memPtr, 1, filesize, f1);             // Check to load directly to memory
-        fread(buffer, 1, filesize, f1);
+        // int* buffer = malloc(filesize);
+        fread(memPtr, 1, filesize, f1);             // Check to load directly to memory
+        // fread(buffer, 1, filesize, f1);
         fclose(f1);
-        memPtr = *buffer;
-        free(buffer);
+        // *memPtr = *buffer;
+        // free(buffer);
     }    
     else
     {
         printf("Could not read file\n");
-        return(0);
+        return;
     }
     fclose(f1);
 }
 
 int main(int argc, char *argv[])
 {
-    int* memPtr = malloc(pow(2,3));
+    int *memPtr = malloc(pow(2,32)/8);
     if(memPtr == NULL)
     {
         printf("No Memory allocated to Interpreter\n");
@@ -44,20 +44,22 @@ int main(int argc, char *argv[])
         return(0);
     }
 
-    readFromFile(argv[1], memPtr[1024]);
-    readFromFile(argv[2], memPtr[256]);
+    printf("%08x", memPtr[1024/8]);
+    readFromFile(argv[1], &memPtr[1024/8]);
+    readFromFile(argv[2], &memPtr[256/8]);
+    printf("\n%08x", memPtr[1024/8]);
 
     if(argc == 4)
     {
-        memPtr[0] = *argv[3];
+        memPtr[0] = atoi(argv[3]);
     }
 
-    int* varStack = memPtr[1048576];                // Variables on Stack Head
-    int* stackHead = memPtr[1048576 + 32*16];       // Execution Stack Head
-    int* PC = memPtr[1024];                         // Program Counter
-    int* CP = memPtr[256];                          // Constant Pool Cunter
+    int *varStack = memPtr+1048576/8;                // Variables on Stack Head
+    int *stackHead = memPtr+(1048576/8 + 4*16);       // Execution Stack Head
+    int *PC = memPtr+1024/8;                         // Program Counter
+    int *CP = memPtr+256/8;                          // Constant Pool Cunter
 
-    printf("StackHead %d", stackHead);
+    // printf("StackHead %d", stackHead);
     // while(stackHead >= 1048576 + 32*16)
     // {
     //     if(PC == CP)
