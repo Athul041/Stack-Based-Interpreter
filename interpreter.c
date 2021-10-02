d4701ff3080f7b416f8b87bf83a9f6dd69570299
Athul Chandran <athul041@gmail.com>
2021-10-02 16:57:01 +0530

    Doing seperate opstack

--- interpreter.c
+++ interpreter.c
@@ -58,74 +58,72 @@ int main(int argc, char *argv[])
     readFromFile(argv[1], &memPtr[1024/8]);
     readFromFile(argv[2], &memPtr[256/8]);
 
-    if(argc == 4)
-    {
-        int num = __builtin_bswap32(atoi(argv[3]));             // Check on linux for big endian
-        memcpy(memPtr,(unsigned char*)&num,4);
-    }
-
-    int stackStart = 1048576 + 4*16;                    // Execution Stack Start
-    int stackHead = stackStart;                         // Execution Stack Head
+    int callStackStart = 1048576 + 4*16;                // Execution Stack Start
+    int stackHead = callStackStart;                     // Execution Stack Head
     int pc = 1024;                                      // Program Counter
     int cp = 256;                                       // Constant Pool Cunter
+    int opStackHead = 1048576;                          // Operand Stack
+    int stackFrameSize = 17;
 
     struct stackFrame sf;
     sf.lva[0] = __builtin_bswap32(atoi(argv[3]));       // Check on linux for big endian
-    while(stackHead >= stackStart)
+    while(stackHead >= callStackStart)
     {
-        printf("\nStackHead %d", stackHead);
-        printf("\npc = %d", pc);
-        printf("\ncp = %d", *(int *) &memPtr[cp]);
-        if(pc == *(int *) &memPtr[cp])
+        printf("\ncp %d", cp);
+        if(cp == 256)
         {
-            // push to stack
-                // stack.pc = *cp
-                // stack.params.count = cp+4
-                // stack.localVars[i] = *(pc+4*i) i<stack.params.count
-            cp += 7;
-            sf.pc = pc;
-            printf("\nsf.pc %d", memPtr[sf.pc]);
+            if(argc == 4)
+            {
+                int num = __builtin_bswap32(atoi(argv[3]));             // Check on linux for big endian
+                memcpy(memPtr+callStackStart,(unsigned char*)&num,4);
+            }
+        }
+        else if(pc == *(int *) &memPtr[cp])
+        {
             for(int i = 0; i < memPtr[cp+4]; i++)
             {
-                sf.lva[i]=__builtin_bswap32(atoi(memPtr+cp+4+i));       // Check on linux for big endian
+                memPtr[stackHead+4*i]=__builtin_bswap32(atoi(memPtr+cp+4+i));       // Check on linux for big endian
             }
             printf("\nparams");
             for(int i = 0; i < memPtr[cp+4]; i++)
             {
-                printf("\n%d", sf.lva[i]);
+                printf("\n%08x", *(memPtr+stackHead+4*i));
             }
-            pushFrameToStack(sf);
         }
-        printf("\nHere %08x", memPtr[sf.pc]);
-        char instr = memPtr[sf.pc];
-        printf("\ninstr %x", instr);
+        memPtr[stackHead] = pc;
+        printf("\npc %d", pc);
+        // printf("\nmemPtr[stackHead] %d", __builtin_bswap32(atoi(memPtr+stackHead)));
+        // printf("\nstackHead %d", stackHead);
+        // printf("\npc %d", memPtr[stackHead]);
+        char instr = memPtr[pc];
+        printf("\ninstr %08x", instr);
         switch(instr){
             case 0x10: // biPush(value)
                 printf("\nBiPush");
-                *sf.opStack = (memPtr[sf.pc+1]);  // Push to op stack
-                sf.pc += 2;                 // Next instruction
+                *sf.opStack = (memPtr[pc+1]);  // Push to op stack
+                pc += 2;                 // Next instruction
             case 0x36: // iStore(index)
                 printf("\niStore");
-                sf.lva[memPtr[sf.pc+1]] = *sf.opStack; // Pop from stack to lva
+                sf.lva[memPtr[pc+1]] = *sf.opStack; // Pop from stack to lva
                 sf.opStack -= 1;
-                sf.pc += 2;
+                pc += 2;
             case 0x15: // iLoad(index)
                 printf("\niLoad");
-                *(sf.opStack+1) = sf.lva[memPtr[sf.pc+1]];
-                sf.pc += 2;
+                *(sf.opStack+1) = sf.lva[memPtr[pc+1]];
+                pc += 2;
             case 0x3: // iConst_0
                 printf("\niconst0");
                 sf.opStack += 1;
                 *sf.opStack = 0;
-                sf.pc += 1;
-            case 0xa7: //GOTO(branchbyte1.branchbyte2) branchbyte1 << 8 | branchbyte2
-                printf("\ngoto");
-                sf.pc = memPtr[sf.pc+1] << 8 | memPtr[sf.pc+1];
-                sf.pc += 2;
-            case 0:
-
+                pc += 1;
+            // case 0xa7: //GOTO(branchbyte1.branchbyte2) branchbyte1 << 8 | branchbyte2
+            //     printf("\ngoto");
+            //     pc = memPtr[pc+1] << 8 | memPtr[pc+1];
+            //     pc += 2;
+            // case 0:
+            //     printf("\n0");
         }
-        char c;
+        char c; 
         scanf("%c",c);
     }
 }
